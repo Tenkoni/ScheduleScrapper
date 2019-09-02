ed1718360970189f7033147d87cf7302a7e80e23
Tenkoni <caliminion@gmail.com>
2019-09-02 11:12:45 -0500

    The URL used to fetch the classes changed, the rest of the format is the same

--- urlGenerator.py
+++ urlGenerator.py
@@ -3,7 +3,7 @@ import sys
 def generateURL(assig_code):
 	try:
 		if assig_code in open("valid_class.miy").read():
-			url = "https://ssa.ingenieria.unam.mx/hrsHtml/" + assig_code + '.html'
+			url = "https://ssa.ingenieria.unam.mx/cj/tmp/programacion_horarios/" + assig_code + '.html'
 			return url
 		return "Invalid class" #código usado para indicar un grupo inexistente
 	except:
